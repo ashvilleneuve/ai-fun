bde05e83c9d943ead521acfaee33bc2ff55283b5
ashvilleneuve <ashley.villeneuve@shopify.com>
2022-05-16 22:10:03 -0700

    onboarding bot first commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import { useState, useCallback, useRef } from "react";
 import enTranslations from '@shopify/polaris/locales/en.json';
 import '@shopify/polaris/build/esm/styles.css';
-import { AppProvider, Button, Avatar, Card, Page, ResourceList, TextStyle } from '@shopify/polaris';
+import { AppProvider, Button, ButtonGroup, Card, Form, FormLayout, Frame, Layout, Loading, Modal, Page, SkeletonBodyText, SkeletonDisplayText, SkeletonPage, TextContainer, TextField, Thumbnail } from '@shopify/polaris';
 
 
 let history;
-if (typeof window !== 'undefined') {
-  console.log('we are running on the client')
-  history = JSON.parse(localStorage.getItem("conversation")) || [];
+if (typeof window !== 'undefined' && localStorage.getItem("conversation") != "") {
+  history = JSON.parse(localStorage.getItem("conversation"));
 } else {
   history = [];
-  console.log('we are running on the server');
 }
 export default function Home() {
   const [userInput, setUserInput] = useState("");
-  const [historyOutput, setHistoryOutput] = useState("");
+  const [historyOutput, setHistoryOutput] = useState(history.reverse());
+  const [isLoading, setIsLoading] = useState(false);
+  const [active, setActive] = useState(true);
+  const loadingMarkup = isLoading ? <Loading /> : null;
+  const skipToContentRef = useRef(null); 
+  const skipToContentTarget = (
+    <a id="SkipToContentTarget" ref={skipToContentRef} tabIndex={-1} />
+  );
+  const handleChange = useCallback(() => setActive(!active), [active]);
+  const activator = useRef(null);
+  function clearHistory() {
+    localStorage.setItem("conversation", "");
+    let clearedHistory = localStorage.getItem("conversation");
+    setHistoryOutput(clearedHistory);
+    handleChange();
+  }
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -27,7 +38,7 @@ export default function Home() {
       },
       body: JSON.stringify({ input: userInput }),
     });
-    const data = await response.json(); 
+    const data = await response.json();
     let lastPrompt = userInput;
     let lastResult = data.result;
     history.push({ prompt: lastPrompt, result: lastResult });
@@ -37,33 +48,103 @@ export default function Home() {
     setUserInput("");
   }
 
+  const logo = {
+    width: 50,
+    topBarSource:
+      '/talk.svg',
+    contextualSaveBarSource:
+      '/talk.svg',
+    url: '/',
+    accessibilityLabel: 'AI Fun',
+  };
+  const historyChildren = historyOutput.length ? historyOutput.map(h => <Card sectioned title={h.prompt}><p>{h.result}</p></Card>) : ""
+  const actualPageMarkup = (
+    <Page 
+      title="Hello, I'm Bit, the Onboarding Buddy Bot."
+      narrowWidth={true}
+    >
+      <Layout>
+        {skipToContentTarget}
+        <Layout.Section>
+          <Form noValidate>
+            <FormLayout>
+              <TextField
+                value={userInput}
+                onChange={(e) => setUserInput(e)}
+                label="How can I help?"
+                type="text"
+                inputMode="text"
+                multiline={4}
+                spellCheck={true}
+                autoComplete="off"
+              />
+              <ButtonGroup>
+                <Button id="activator" onClick={handleChange} ref={activator}>Clear history</Button>
+                <Button primary onClick={onSubmit}>Submit</Button>
+              </ButtonGroup>
+            </FormLayout>
+          </Form>
+        </Layout.Section>
+        <Layout.Section 
+          className="history container"
+          children={historyChildren}
+        >
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+
+  const loadingPageMarkup = (
+    <SkeletonPage>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <TextContainer>
+              <SkeletonDisplayText size="small" />
+              <SkeletonBodyText lines={9} />
+            </TextContainer>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </SkeletonPage>
+  );
+
+  const modal = (<div>
+    <Modal
+      activator={activator}
+      open={!active}
+      onClose={handleChange}
+      title="Are you sure?"
+      primaryAction={{
+        content: 'Delete history',
+        onAction: clearHistory,
+      }}
+      secondaryActions={[
+        {
+          content: 'Cancel',
+          onAction: handleChange,
+        },
+      ]}
+    >
+      <Modal.Section>
+        <p>Clearing chat history is permanent.</p>
+      </Modal.Section>
+    </Modal>
+  </div>
+  );
+
+  const pageMarkup = isLoading ? loadingPageMarkup : actualPageMarkup;
+
   return (
     <AppProvider i18n={enTranslations}>
-      <Page>
-        <Head>
-          <title>OpenAI Quickstart</title>
-          <link rel="icon" href="/talk.svg" />
-        </Head>
-
-        <main className={styles.main}>
-          <img src="/talk.svg" className={styles.icon} />
-          <h3>Say something</h3>
-          <form>
-            <textarea
-              name="user"
-              rows="4"
-              cols="50"
-              placeholder="Say something"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-            />
-            <Button onClick={onSubmit}>Send</Button>
-          </form>
-          <div className="history container">
-            {historyOutput.length ? historyOutput.map(h => (<Card title={h.prompt}><p>{h.result}</p></Card>)) : <Card title="Welcome"></Card>} 
-          </div>
-        </main>
-      </Page>
+        <Frame
+          logo={logo}
+          skipToContentTarget={skipToContentRef.current}
+        >
+        {modal}
+        {loadingMarkup}
+        {pageMarkup}
+        </Frame>
     </AppProvider>
   );
 }
