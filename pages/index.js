abaa6e7c3ce17f8d811553b5c05a2a0aeda8b8a9
ashvilleneuve <ashley.villeneuve@shopify.com>
2022-05-16 23:50:20 -0700

    trying this fix again

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ input: userInput }),
+      body: JSON.stringify({ "input": userInput }),
     });
     const data = await response.json();
     let lastPrompt = userInput;
