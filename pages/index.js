724d4c7a88b32ee7f27795e18c118cd5eca882f2
ashvilleneuve <ashley.villeneuve@shopify.com>
2022-05-22 03:03:52 -0700

    load states, api refinements, better layout

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,14 @@ import {
   FormLayout, 
   Frame, 
   Layout, 
+  Loading,
   Modal, 
   Page, 
-  Stack,
+  SkeletonBodyText,
+  SkeletonDisplayText,
   TextField, 
   TextStyle,
-  Thumbnail 
+  Toast,
 } from '@shopify/polaris';
 
 
@@ -26,7 +28,9 @@ export default function Home() {
     history = JSON.parse(localStorage.getItem("conversation"));
   } else {
     history = [];
-  }
+  } 
+  const [toastActive, setToastActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [userInput, setUserInput] = useState("");
   const [historyOutput, setHistoryOutput] = useState(history.reverse());
   const [active, setActive] = useState(true);
@@ -36,6 +40,7 @@ export default function Home() {
     <a id="SkipToContentTarget" ref={skipToContentRef} tabIndex={-1} />
   );
   const activator = useRef(null);
+
   function clearHistory() {
     localStorage.setItem("conversation", "");
     let clearedHistory = localStorage.getItem("conversation");
@@ -43,15 +48,24 @@ export default function Home() {
     handleChange();
   }
 
+  function promptHandler(e) {
+    if (e.includes('\n')) {
+      onSubmit(e);
+    } else {
+      setUserInput(e)
+    }
+  }
+
   async function onSubmit(event) {
-    event.preventDefault();
+    setToastActive(true);
+    setIsLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ input: userInput }),
-    });
+    }); 
     const data = await response.json();
     let lastPrompt = userInput;
     let lastResult = data.result;
@@ -60,44 +74,58 @@ export default function Home() {
     let getHistory = JSON.parse(localStorage.getItem("conversation"));
     setHistoryOutput(getHistory.reverse());
     setUserInput("");
+    toggleIsLoading();
   }
 
-  const logo = {
-    width: 75,
-    source:
-      '/public/robot.png',
-    accessibilityLabel: 'Geoffrey the Goaliebot',
-  };
-  const historyChildren = historyOutput.length ? historyOutput.map(h => <Card sectioned key={h.prompt} title={"Question: " + h.prompt}><p><TextStyle variation="strong">Response: </TextStyle>{h.result}</p></Card>) : "";
+  const toggleToastActive = useCallback(
+    () => setToastActive((toastActive) => !toastActive),
+    [],
+  );
+  const toggleIsLoading = useCallback(
+    () => setIsLoading((isLoading) => !isLoading),
+    [],
+  );
+
+  const toastMarkup = toastActive ? (
+    <Toast onDismiss={toggleToastActive} content="Question submitted" />
+  ) : null;
+
+  const loadingMarkup = isLoading ? <Loading /> : null; 
+  const actualHistoryChildren = historyOutput.length ? historyOutput.map(h => <Card sectioned key={h.prompt} title={"Question: " + h.prompt}><p><TextStyle variation="strong">Response: </TextStyle>{h.result}</p></Card>) : "";
+  const loadingHistoryChildren = [...Array(historyOutput.length+1)].map((h) => <Card sectioned><p><SkeletonDisplayText size="small" /><SkeletonBodyText lines={4} /></p></Card>);
+  const historyChildren = isLoading ? loadingHistoryChildren : actualHistoryChildren;
+
   const pageMarkup = (
     <Page 
       narrowWidth={true}
     >
-      <Stack distribution="trailing">
-        <Stack.Item fill>
+      <Layout distribution="fill">
+        <Layout.Section oneThird>
           <DisplayText size="extraLarge">Hello, I'm Geoffrey, the Goaliebot.</DisplayText>
-        </Stack.Item>
-        <Stack.Item>
-          <Thumbnail
-            source="/robot.png"
-            size="large"
-            transparent={true}
-            alt="Geoffrey the Goaliebot"
-          />
-        </Stack.Item>
-      </Stack>
+        </Layout.Section>
+        <Layout.Section oneThird>
+          <img src="../robot.png" alt="Geoffrey the Goaliebot" width='150px'></img>
+        </Layout.Section>
+      </Layout>
       <Layout>
         {skipToContentTarget}
         <Layout.Section>
-          <Form noValidate>
+          <Form
+            noValidate
+            implicitSubmit={true}
+          >
             <FormLayout>
+              <DisplayText size="medium">How can I help?</DisplayText>
               <TextField
+                ariaActiveDescendant="activator"
+                ariaControls="history_container"
                 value={userInput}
-                onChange={(e) => setUserInput(e)}
+                onChange={(e) => promptHandler(e)}
                 label="How can I help?"
+                labelHidden={true}
                 type="text"
                 inputMode="text"
-                multiline={4}
+                multiline={3}
                 spellCheck={true}
                 autoComplete="off"
               />
@@ -110,7 +138,9 @@ export default function Home() {
         </Layout.Section>
         <Layout.Section 
           className="history container"
+          id="history_container"
         >
+          {loadingMarkup}
           {historyChildren}
         </Layout.Section>
       </Layout>
@@ -144,11 +174,11 @@ export default function Home() {
   return (
     <AppProvider i18n={enTranslations}>
         <Frame
-          logo={logo}
           skipToContentTarget={skipToContentRef.current}
         >
         {pageMarkup}
         {modal}
+        {toastMarkup}
         </Frame>
     </AppProvider>
   );
